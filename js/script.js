e843de64be68f444f1ced90574e96fdfe15c87d2
Sharmarke Said <sharmakesaid17@gmail.com@gmail.com>
2023-09-13 11:35:18 +0300

    Implemented a smooth scrooling for all browsers

--- js/script.js
+++ js/script.js
@@ -23,35 +23,62 @@ yearEl.textContent = currentYear;
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener("click", function () {
+btnNavEl.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
 });
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
-const allLinks = document.querySelectorAll("a:link");
+// const allLinks = document.querySelectorAll("a:link");
 
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute("href");
+
+//     // Scroll back to top
+//     if (href === "#")
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//       });
+
+//     // Scroll to other links
+//     if (href !== "#" && href.startsWith("#")) {
+//       const sectionEl = document.querySelector(href);
+//       sectionEl.scrollIntoView({ behavior: "smooth" });
+//     }
+
+//     // Close mobile naviagtion
+//     if (link.classList.contains("main-nav-link"))
+//       headerEl.classList.toggle("nav-open");
+//   });
+// });
+
+allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
     e.preventDefault();
+
     const href = link.getAttribute("href");
 
-    // Scroll back to top
-    if (href === "#")
+    // scrolling to top
+    if (href === "#") 
       window.scrollTo({
         top: 0,
-        behavior: "smooth",
-      });
+        behavior: "smooth"
+    });
 
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
+    // Scroll to ther links
+    if(href!=="#" && href.startsWith("#")){
+      const sectionEl = document.querySelector(href)
+      // console.log(sectionEl)
+      sectionEl.scrollIntoView({behavior: "smooth"})
     }
 
-    // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
+    // close mobile navigation
+    if(link.classList.contains("main-nav-link"))
       headerEl.classList.toggle("nav-open");
   });
 });
@@ -72,7 +99,9 @@ const obs = new IntersectionObserver(
 
     if (ent.isIntersecting === true) {
       document.body.classList.remove("sticky");
+      
     }
+
   },
   {
     // In the viewport
