a7142e32a5c46bac9871984d8ad7f1875564f531
Sharmarke Said <sharmakesaid17@gmail.com@gmail.com>
2023-09-14 09:04:23 +0300

    Updated the img folder

--- js/script.js
+++ js/script.js
@@ -64,73 +64,91 @@ allLinks.forEach((link) => {
     const href = link.getAttribute("href");
 
     // scrolling to top
-    if (href === "#") 
+    if (href === "#")
       window.scrollTo({
         top: 0,
-        behavior: "smooth"
-    });
+        behavior: "smooth",
+      });
 
     // Scroll to ther links
-    if(href!=="#" && href.startsWith("#")){
-      const sectionEl = document.querySelector(href)
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
       // console.log(sectionEl)
-      sectionEl.scrollIntoView({behavior: "smooth"})
+      sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
     // close mobile navigation
-    if(link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
+    if (link.classList.contains("main-nav-link")) headerEl.classList.toggle("nav-open");
   });
 });
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
 
+// const sectionHeroEl = document.querySelector(".section-hero");
+
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+//     console.log(ent);
+
+//     if (ent.isIntersecting === false) {
+//       document.body.classList.add("sticky");
+//     }
+
+//     if (ent.isIntersecting === true) {
+//       document.body.classList.remove("sticky");
+
+//     }
+
+//   },
+//   {
+//     // In the viewport
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-80px",
+//   }
+// );
+// obs.observe(sectionHeroEl);
+
 const sectionHeroEl = document.querySelector(".section-hero");
 
 const obs = new IntersectionObserver(
-  function (entries) {
+  (entries) => {
     const ent = entries[0];
     console.log(ent);
-
-    if (ent.isIntersecting === false) {
+    if (!ent.isIntersecting) 
       document.body.classList.add("sticky");
-    }
-
-    if (ent.isIntersecting === true) {
+    if (ent.isIntersecting) 
       document.body.classList.remove("sticky");
-      
-    }
-
   },
   {
-    // In the viewport
     root: null,
     threshold: 0,
-    rootMargin: "-80px",
+    rootMargin: "-80px"
   }
 );
 obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
-function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+// function checkFlexGap() {
+//   var flex = document.createElement("div");
+//   flex.style.display = "flex";
+//   flex.style.flexDirection = "column";
+//   flex.style.rowGap = "1px";
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
 
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
+//   document.body.appendChild(flex);
+//   var isSupported = flex.scrollHeight === 1;
+//   flex.parentNode.removeChild(flex);
+//   console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
-}
-checkFlexGap();
+//   if (!isSupported) document.body.classList.add("no-flexbox-gap");
+// }
+// checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
